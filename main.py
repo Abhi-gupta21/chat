a021a8ae8170d497ef482e8afe0e2a4ed53b7636
Ketepally <Sai.Ketepally@cchmc.org>
2025-02-05 10:00:06 -0500

    added speech recognition

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ audio_input = st.audio_input("audio input")
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-medium"
+model_id = "openai/whisper-base"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
