becd1b139360b6f7dea3eedae79bcef1a0aa4a53
Ketepally <Sai.Ketepally@cchmc.org>
2025-02-04 19:53:32 -0500

    added whisper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+from pydub import AudioSegment
 import streamlit as st
+import torch
+from transformers import AutoTokenizer, AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from langchain_community.vectorstores import FAISS
 from langchain_openai import AzureOpenAIEmbeddings
 from langchain_community.chat_message_histories import ChatMessageHistory
@@ -15,6 +18,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
@@ -31,6 +35,8 @@ llm = AzureChatOpenAI(
     timeout = None,
     max_retries = 2,
 )
+
+language = 'english'
 
 embeddings = AzureOpenAIEmbeddings(
     model="text-embedding-3-large",
@@ -68,12 +74,14 @@ history_aware_retriever = create_history_aware_retriever(llm, retriever, context
 system_prompt = (
     "You are an assistant for question answering tasks."
     "You will be provided with a question and a context."
-    "You will answer the question based on the context."
+    "You will answer the question based on the context in {language}."
     "If the context is not relevant, just say that you don't know and you are not made to answer such qustions."
     "If you don't know the answer, just say that you don't know, don't try to make up an answer."
     "\n\n"
     "{context}"
 )
+
+
 
 qa_prompt = ChatPromptTemplate.from_messages(
     [
@@ -101,11 +109,65 @@ conversational_rag_chain = RunnableWithMessageHistory(
 )
 
 user_input = st.text_input("your question:")
+# add a function to take in language through whisper and set language to that
+
+audio_input = st.audio_input("audio input")
+
 
 if user_input:
     session_history = get_session_history(session_id)
     response = conversational_rag_chain.invoke(
-        {"input": user_input},
+        {
+            "input": user_input,
+            "language": language,
+        },
+        config={"configurable": 
+                {
+                    "session_id": session_id
+                }
+            })
+    st.write(st.session_state.store)
+    st.success(f"Assistant: {response['answer']}")
+    st.write("Chat history:", session_history.messages)
+
+
+if audio_input:
+    st.audio(audio_input)
+    audio_wav = AudioSegment.from_wav(audio_input)
+    audio_wav.export("audio.mp3", format="mp3")
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = "openai/whisper-large-v3"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+
+
+    result = pipe("audio.mp3", generate_kwargs={"task": "translate"})
+    st.write(result["text"])
+
+    input = result["text"]
+    session_history = get_session_history(session_id)
+    response = conversational_rag_chain.invoke(
+        {
+            "input": input,
+            "language": language,
+        },
         config={"configurable": 
                 {
                     "session_id": session_id
